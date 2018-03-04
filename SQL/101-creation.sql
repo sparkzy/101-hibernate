f565539bf1eda565f742ab1d1bdd70b1b89eca74
Bobby M <31968082+sparkzy@users.noreply.github.com>
2018-03-04 14:31:55 -0500

    re-pushing

--- SQL/101-creation.sql
+++ SQL/101-creation.sql
@@ -52,28 +52,78 @@ CREATE TABLE "101_role"
     role_id NUMBER PRIMARY KEY,
     role VARCHAR(100) UNIQUE NOT NULL
 );
---
---CREATE TABLE ers_reimbursement_status
+
+CREATE TABLE "101_post"
+(
+    post_id NUMBER PRIMARY KEY,
+    title VARCHAR2(50) NOT NULL,
+    user_id NUMBER,
+    body VARCHAR2(2000),
+--    content BLOB,
+    likes NUMBER,
+    status_id NUMBER
+--    first_comment NUMBER
+);
+
+CREATE TABLE "101_subject"
+(
+    subject_id NUMBER PRIMARY KEY,
+    subject_name VARCHAR2(50)
+);
+
+CREATE TABLE "101_status"
+(
+    status_id NUMBER PRIMARY KEY,
+    status_name VARCHAR2(50)
+);
+
+CREATE TABLE "101_quiz"
+(
+    quiz_id NUMBER PRIMARY KEY,
+    subject_id NUMBER,
+--    avg_score/diffculty NUMBER
+    likes NUMBER,
+    title VARCHAR2(30) NOT NULL,
+    author_id NUMBER
+);
+
+CREATE TABLE "101_question"
+(
+    question_id NuMBER PRIMARY KEY,
+    quiz_id NUMBER,
+    correct_answer VARCHAR(200) NOT NULL,
+    wrong_answer_0 VARCHAR2(200) NOT NULL,
+    wrong_answer_1 VARCHAR2(200) NOT NULL,
+    wrong_answer_2 VARCHAR2(200) NOT NULL
+);
+
+CREATE TABLE "101_fc_sect"
+(
+    
+);
+
+CREATE TABLE "101_flashcard"
+(
+    
+);
+
+--CREATE TABLE "101_comment"
 --(
---    reimb_status_id NUMBER PRIMARY KEY NOT NULL,
---    reimb_status VARCHAR2(10) NOT NULL
---);
---
---CREATE TABLE ers_reimbursement_type
---(
---    reimb_type_id NUMBER PRIMARY KEY NOT NULL,
---    reimb_type VARCHAR2(10) NOT NULL
---);
---
---CREATE TABLE ers_user_roles
---(
---    ers_user_role_id NUMBER PRIMARY KEY NOT NULL,
---    user_role VARCHAR2(30) NOT NULL
+--    
 --);
 
 /*******************************************************************************
    Create Junction Tables
 ********************************************************************************/
+CREATE TABLE "101_post_to_subject"
+(
+    
+);
+
+CREATE TABLE "101_fc_to_set"
+(
+    
+);
 
 
 /*******************************************************************************
@@ -89,21 +139,19 @@ CREATE TABLE "101_role"
 **/
 ALTER TABLE "101_user" ADD CONSTRAINT "101_role_id_fk_auth"
     FOREIGN KEY (role_id) REFERENCES "101_role" (role_id) ON DELETE CASCADE;
---    
---ALTER TABLE ers_reimbursement ADD CONSTRAINT ers_user_fk_reslvr
---    FOREIGN KEY (reimb_resolver) REFERENCES ers_users (ers_user_id) ON DELETE CASCADE;
---
---ALTER TABLE ers_reimbursement ADD CONSTRAINT ers_reimbursement_status_fk
---    FOREIGN KEY (reimb_status_id) REFERENCES ers_reimbursement_status (reimb_status_id) ON DELETE CASCADE;
---    
---ALTER TABLE ers_reimbursement ADD CONSTRAINT ers_reimbursement_type_fk
---    FOREIGN KEY (reimb_type_id) REFERENCES ers_reimbursement_type (reimb_type_id) ON DELETE CASCADE;
     
 /**
-* ers_users
+* 101_post
 **/
---ALTER TABLE ers_users ADD CONSTRAINT ers_user_roles_fk
---    FOREIGN KEY (user_role_id) REFERENCES ers_user_roles (ers_user_role_id) ON DELETE CASCADE;
+ALTER TABLE "101_psot" ADD CONSTRAINT "101_user_id_fk_auth"
+    FOREIGN KEY (user_id) REFERENCES "101_user" (user_id) ON DELETE CASCADE;
+    
+/**
+* 101_quiz
+**/
+ALTER TABLE "101_quiz" ADD CONSTRAINT "101_author_id_fk_auth"
+    FOREIGN KEY (author_id) REFERENCES "101_user" (user_id) ON DELETE CASCADE;
+
 
 /*******************************************************************************
    Create Sequences
@@ -111,12 +159,22 @@ ALTER TABLE "101_user" ADD CONSTRAINT "101_role_id_fk_auth"
 CREATE SEQUENCE "101_user_id_seq";
 
 CREATE SEQUENCE "101_role_id_seq";
---
---CREATE SEQUENCE reimb_status_id_seq;
---
---CREATE SEQUENCE reimb_type_id_seq;
---
---CREATE SEQUENCE ers_user_role_id_seq;
+
+CREATE SEQUENCE "101_post_id_seq";
+
+CREATE SEQUENCE "101_subject_id_seq";
+
+CREATE SEQUENCE "101_status_id_seq";
+
+CREATE SEQUENCE "101_quiz_id_seq";
+
+CREATE SEQUENCE "101_question_id_seq";
+
+CREATE SEQUENCE "101_fc_set_id_seq";
+
+CREATE SEQUENCE "101_flashcard_id_seq";
+
+--CREATE SEQUENCE "101_comment_id_seq";
 
 /*******************************************************************************
    Create Triggers
@@ -126,7 +184,7 @@ CREATE OR REPLACE TRIGGER user_id_trig
     FOR EACH ROW
     BEGIN
         IF INSERTING THEN
-            SELECT useer_id_seq.nextVal INTO :new.user_id FROM DUAL;
+            SELECT user_id_seq.nextVal INTO :new.user_id FROM DUAL;
         ELSIF UPDATING THEN
             SELECT :old.user_id INTO :new.user_id FROM DUAL;
         END IF;
@@ -144,42 +202,103 @@ CREATE OR REPLACE TRIGGER role_id_trig
         END IF;
     END;
     /
---    
---CREATE OR REPLACE TRIGGER reimb_status_id_trig
---    BEFORE INSERT OR UPDATE ON ers_reimbursement_status
---    FOR EACH ROW
---    BEGIN
---        IF INSERTING THEN
---            SELECT reimb_status_id_seq.nextVal INTO :new.reimb_status_id FROM DUAL;
---        ELSIF UPDATING THEN
---            SELECT :old.reimb_status_id INTO :new.reimb_status_id FROM DUAL;
---        END IF;
---    END;
---    /
---    
---CREATE OR REPLACE TRIGGER reimb_type_id_trig
---    BEFORE INSERT OR UPDATE ON ers_reimbursement_type
---    FOR EACH ROW
---    BEGIN
---        IF INSERTING THEN
---            SELECT reimb_type_id_seq.nextVal INTO :new.reimb_type_id FROM DUAL;
---        ELSIF UPDATING THEN
---            SELECT :old.reimb_type_id INTO :new.reimb_type_id FROM DUAL;
---        END IF;
---    END;
---    /
---    
---CREATE OR REPLACE TRIGGER ers_user_role_id_trig
---    BEFORE INSERT OR UPDATE ON ers_user_roles
---    FOR EACH ROW
---    BEGIN
---        IF INSERTING THEN
---            SELECT ers_user_role_id_seq.nextVal INTO :new.ers_user_role_id FROM DUAL;
---        ELSIF UPDATING THEN
---            SELECT :old.ers_user_role_id INTO :new.ers_user_role_id FROM DUAL;
---        END IF;
---    END;
---    /
+
+CREATE OR REPLACE TRIGGER post_id_trig
+    BEFORE INSERT OR UPDATE ON "101_post"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT post_id_seq.nextVal INTO :new.post_id FROM DUAL;
+            SELECT 0 INTO :new.like FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.post_id INTO :new.post_id FROM DUAL;
+        END IF;
+    END;
+    /
+
+CREATE OR REPLACE TRIGGER subject_id_trig
+    BEFORE INSERT OR UPDATE ON "101_subject"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT subject_id_seq.nextVal INTO :new.subject_id FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.subject_id INTO :new.subject_id FROM DUAL;
+        END IF;
+    END;
+    /
+    
+CREATE OR REPLACE TRIGGER status_id_trig
+    BEFORE INSERT OR UPDATE ON "101_status"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT status_id_seq.nextVal INTO :new.status_id FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.status_id INTO :new.status_id FROM DUAL;
+        END IF;
+    END;
+    /
+    
+CREATE OR REPLACE TRIGGER quiz_id_trig
+    BEFORE INSERT OR UPDATE ON "101_quiz"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT quiz_id_seq.nextVal INTO :new.quiz_id FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.quiz_id INTO :new.quiz_id FROM DUAL;
+        END IF;
+    END;
+    /
+    
+CREATE OR REPLACE TRIGGER question_id_trig
+    BEFORE INSERT OR UPDATE ON "101_question"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT question_id_seq.nextVal INTO :new.question_id FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.question_id INTO :new.question_id FROM DUAL;
+        END IF;
+    END;
+    /
+    
+CREATE OR REPLACE TRIGGER fc_set_id_trig
+    BEFORE INSERT OR UPDATE ON "101_user"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT fc_set_seq.nextVal INTO :new.fc_set_id FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.fc_set_id INTO :new.fc_set_id FROM DUAL;
+        END IF;
+    END;
+    /
+    
+CREATE OR REPLACE TRIGGER flashcard_id_trig
+    BEFORE INSERT OR UPDATE ON "101_user"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT flashcard_id_seq.nextVal INTO :new.flashcard_id FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.flashcard_id INTO :new.flashcard_id FROM DUAL;
+        END IF;
+    END;
+    /
+    
+CREATE OR REPLACE TRIGGER comment_id_trig
+    BEFORE INSERT OR UPDATE ON "101_comment"
+    FOR EACH ROW
+    BEGIN
+        IF INSERTING THEN
+            SELECT comment_id_seq.nextVal INTO :new.comment_id FROM DUAL;
+        ELSIF UPDATING THEN
+            SELECT :old.comment_id INTO :new.comment_id FROM DUAL;
+        END IF;
+    END;
+    /
 
 /*******************************************************************************
    Create Stored Procedures
@@ -198,25 +317,6 @@ CREATE OR REPLACE TRIGGER role_id_trig
    Initialize Tables
 ********************************************************************************/
 --INSERT INTO ers_user_roles (user_role) VALUES ('Employee');
---INSERT INTO ers_user_roles (user_role) VALUES ('Finance Manager');
---
---INSERT INTO ers_users (ers_username, ers_password, user_first_name, user_last_name, user_email, user_role_id) 
---    VALUES ('admin', 'adminPass', 'admin', 'admin', 'admin@mail.com', 2);    
---INSERT INTO ers_users (ers_username, ers_password, user_first_name, user_last_name, user_email, user_role_id) 
---    VALUES ('emp', 'empPass', 'emp', 'ployee', 'emp@mail.com', 1);
---INSERT INTO ers_users (ers_username, ers_password, user_first_name, user_last_name, user_email, user_role_id) 
---    VALUES ('bman2000', 'coolObjects', 'Blake', 'Kruppa', 'blake@revature.com', 2);
---INSERT INTO ers_users (ers_username, ers_password, user_first_name, user_last_name, user_email, user_role_id) 
---    VALUES ('sparkzy', 'beer', 'Bobby', 'McGetrick', 'bobby@revature.com', 1);
---
---INSERT INTO ers_reimbursement_status (reimb_status) VALUES ('Pending');
---INSERT INTO ers_reimbursement_status (reimb_status) VALUES ('Declined');
---INSERT INTO ers_reimbursement_status (reimb_status) VALUES ('Approved');
---
---INSERT INTO ers_reimbursement_type (reimb_type) VALUES ('Food');
---INSERT INTO ers_reimbursement_type (reimb_type) VALUES ('Lodging');
---INSERT INTO ers_reimbursement_type (reimb_type) VALUES ('Travel');
---INSERT INTO ers_reimbursement_type (reimb_type) VALUES ('Other');
 
 /*******************************************************************************
    Commit changes and exit
